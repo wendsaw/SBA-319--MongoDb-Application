88c723dadbaa38c65b65cc6f1a8e2b26b2448cc6
wendsaw <sawadogo.edgard@gmail.com>
2025-02-01 12:49:36 -0700

    update

---
+++ Models/books.js
@@ -0,0 +1,50 @@
+const mongoose=require('mongoose');
+const Schema=mongoose.Schema;
+
+
+const bookSchema=new Schema({
+
+    title:{
+        type:String,
+        required:true
+    },
+    author:{
+        type:String,
+        required:true
+    
+    },
+    pages:{
+        type:Number,
+        required:true
+    },
+    genres:{
+        type:String,
+        required:true
+    },
+    published:{
+        type:Date,
+        required:true
+    },
+
+    rating:{
+        type:Number,
+        required:true,
+        min:1,
+        max:10,
+
+    },
+    words:{
+        type:Number,
+        required:true
+    },
+    language:{
+        type:String,
+        required:true
+    },
+
+
+}, {timestamps:true})
+
+const Book=mongoose.model('book', bookSchema);
+
+module.exports= Book
--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express =require ('express');
 const mongoose=require ('mongoose');
 const env=require('dotenv/config')
+const Book=require('./Models/books')
 
 
 
@@ -33,6 +34,33 @@ mongoose.connect(process.env.URL).then(()=>{
 app.get('/',(req,res)=>{
 
     res.send('home page')
+})
+
+//add books to the data base
+
+app.post('/books/addbooks', (req,res)=>{
+    const book = new Book({
+        title: req.body.title,
+        author: req.body.author,
+        pages: req.body.pages,
+        genres:req.body.genres,
+        published:req.body.published,
+        rating:req.body.rating,
+        words:req.body.words,
+        language:req.body.language,
+    
+    })
+    console.log(book)
+    book.save()
+    .then((result)=>{
+        res.send(result)
+    }).catch((err)=>{
+
+        res.status(500).send(err)
+
+    })
+
+
 })
 
 
